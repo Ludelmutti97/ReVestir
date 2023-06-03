486269f4e3801a553854c70890aa3135858dfc27
Ludelmutti97 <127596877+Ludelmutti97@users.noreply.github.com>
2023-06-03 20:18:54 +0100

    Update auth.js

--- BackEnd/controller/service/auth.js
+++ BackEnd/controller/service/auth.js
@@ -1,4 +1,4 @@
-const { findProdutos, findfavoriteById, findFavoriteAll } = require("../utils/auth");
+const { findProdutos, findfavoriteById, findFavoriteAll,findTop,findFilter, findSearchBox , findCategory } = require("../utils/auth");
 const { findProductById } = require("../utils/auth");
 
 async function getProduto() {
@@ -17,8 +17,69 @@ async function getProdutoById(id) {
 async function getFavorite() {
   const pedido = await findFavoriteAll();
   return pedido;
+
+}
+
+// Add Outfit FAZER----------------------------------------------
+
+async function ghghg() {
+  const pedido = await ghgh();
+  return pedido;
+
+}
+
+//----------------------------------------------------------------
+
+
+
+// ver com o RAFA----------------------------------------------
+async function getAddProductReview(productId, review) {
+  
+    const product = await findReview(productId);
+
+    if (product) {
+      product.reviews.push(review);
+      product.numReviews = product.reviews.length;
+
+      await collectionName.updateOne(
+        { _id: ObjectId(productId) },
+        { $set: product }
+      )
+
+      return 
+    }
+  
+}
+//-----------------------------------------------------------
+
+
+
+async function getTopRatedProducts() {
+  const pedido = await findTop();
+  return pedido;
+
+}
+
+async function getCreateCategory() {
+  const pedido = await findCategory();
+  return pedido;
+
+}
+
+async function getFilterProductsByCategory() {
+  const pedido = await findFilter();
+  return pedido;
+
+}
+
+async function getsearchProducts() {
+  const pedido = await findSearchBox();
+  return pedido;
+
 }
 
 
 
-module.exports = { getProduto, getProdutoId , getProdutoById,  getFavorite};
+
+module.exports = { getProduto, getProdutoId , getProdutoById,  getFavorite, getAddProductReview,getTopRatedProducts, 
+  getCreateCategory, getFilterProductsByCategory, getsearchProducts};
