ffda140222d3c9b1e275455a1a8c893fef9b328f
Ludelmutti97 <127596877+Ludelmutti97@users.noreply.github.com>
2023-06-03 20:17:39 +0100

    Update auth.js

--- BackEnd/controller/utils/auth.js
+++ BackEnd/controller/utils/auth.js
@@ -2,6 +2,8 @@ const { ObjectId } = require("mongodb");
 const { getMongoCollection } = require("./baseDados");
 
 const collectionName = "produtos";
+const collectionCategory= "categorias"
+
 
 async function findProdutos() {
   const collection = await getMongoCollection(collectionName);
@@ -20,9 +22,9 @@ async function findfavoriteById(id) {
   const produtoFavorito = await collection.findOneAndUpdate(
     { _id: new ObjectId(id) },
     [
-     {$set: {favorite: {$not: "$favorite"}}}
+      { $set: { favorite: { $not: "$favorite" } } }
     ]
-     );
+  );
 
   return produtoFavorito;
 }
@@ -38,10 +40,82 @@ async function findFavoriteAll() {
   return produtoFavorito;
 }
 
-module.exports = {
-  findProdutos,
-  findProductById,
-  findfavoriteById,
-  findFavoriteAll
-};
-//module.exports = { FavoritePorId };
+// OUTFIT - Ver com o Rafa como faz, tentar entender--------------------------
+
+
+
+
+
+
+// -ESCREVER AQUI !!!!!---------------------------------------------
+
+
+
+
+//                    ***Ver com o RAFA****
+// CRIAR AVALIAÇÂO 
+async function findReview(productId) {
+
+  const collection = await getMongoCollection(collectionName);
+  const avaliacao = await collection.findOne({ _id: ObjectId(productId) })
+
+  return avaliacao;
+  
+}
+
+
+  //Filtro pela avaliação dos melhores
+
+  async function findTop() {
+
+    const collection = await getMongoCollection(collectionName);
+    const products = await collection.find({}).sort({ rating: -1 }).limit(3).toArray();
+    return products;
+
+  }
+  //Categorias
+  async function findCategory(name, color, gender, material, type, description) {
+    
+    const collection = await getMongoCollection(collectionCategory);
+    const category = {
+      name,
+      color,
+      gender,
+      material,
+      type,
+      description,
+    };
+    const result = await collection.insertOne(category); 
+    return result.ops[0];  //Executa a inserção de um documento na coleção e retorna o documento inserido como resultado da operação.
+  
+  }
+  // Filtro todas as categorias
+
+  async function findFilter(category) {
+    const collection = await getMongoCollection(collectionCategory);
+    const filteredProducts = await collection.find({ category }).toArray();
+
+
+    return filteredProducts;
+  }
+
+  // Barra de Pesquisa
+  async function findSearchBox(termo) {
+
+    const collection = await getMongoCollection(collectionName);
+
+    const products = await collection.find({ nome: { $regex: termo, $options: "i" } }).toArray();
+    return products;
+  }
+
+  module.exports = {
+    findProdutos,
+    findProductById,
+    findfavoriteById,
+    findFavoriteAll,
+    findReview,
+    findTop,
+    findFilter,
+    findSearchBox,
+    findCategory
+  }
