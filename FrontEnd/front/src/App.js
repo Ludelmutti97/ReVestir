5c828d9226829d343bbe8d6217c2f626ce84acb3
Luis Filipe Caetano Almeida <Luis@MacBook-Pro-de-Luis-Almeida.local>
2023-06-05 16:07:12 +0100

    rotas

--- FrontEnd/front/src/App.js
+++ FrontEnd/front/src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./componentes/HomePage/HomePage";
-import Carousel from "./componentes/carrosel/Carousel";
+import Teste1 from "./componentes/teste/teste1";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-     <HomePage/>
-     <div>
-      <Carousel/>
-        
-     </div>
-     
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<HomePage/>} />
+        <Route path="/produtos" element={<Teste1/>} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
---
+++ FrontEnd/front/src/componentes/teste/teste1.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Teste1 (){
+    return(
+        <h1>Hello do teste1</h1>
+    )
+}
+
+export default Teste1
