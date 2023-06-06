04315156940bc3b491a7cf1df85b4edf5bed1c30
Luanna Delmuti <ludelmutti@gmail.com>
2023-06-06 16:02:22 +0100

    Luanna

--- FrontEnd/front/src/componentes/luana/luana.js
+++ FrontEnd/front/src/componentes/luana/luana.js
@@ -8,9 +8,13 @@ export function Luana() {
 
   const handleClick = async () => {
     try {
-      const response = await fetch("/api/favorite/:id");
+      console.log("FETCHING");
+      const response = await fetch("/api/favorite/647da6d2ff948d77053693f8", {
+        method: "POST",
+      });
       if (response.ok) {
         const data = await response.json();
+        console.log(data);
         setIsFavorite(true);
         return data;
       }
@@ -24,18 +28,25 @@ export function Luana() {
       <button onClick={handleClick} className="favorite-button">
         {isFavorite ? "Favorited" : "❤️"}
       </button>
-      <AddToOutfitButton />
-      <SearchBar />
+
+      <div className="formata">
+        <AddToOutfitButton productId="647da6d2ff948d77053693f8" />
+        <RemoveToOutfitButton productId="647da6d2ff948d77053693f8" />
+        <SearchBar />
+        <FetchAllProductsButton />
+        <Sidebar />
+
+      </div>
     </div>
   );
 }
 
 export function AddToOutfitButton(productId) {
   const [message, setMessage] = useState("");
-
+  console.log(productId.productId);
   const handleClick = async () => {
     try {
-      const response = await fetch(`/api/outfit/${productId}`, {
+      const response = await fetch(`/api/outfit/${productId.productId}`, {
         method: "POST",
       });
       if (response.ok) {
@@ -46,8 +57,10 @@ export function AddToOutfitButton(productId) {
     } catch (err) {
       console.log(err);
     }
+    
   };
 
+  
   return (
     <div>
       <button onClick={handleClick} className="outFit">
@@ -57,6 +70,38 @@ export function AddToOutfitButton(productId) {
     </div>
   );
 }
+
+
+export function RemoveToOutfitButton(productId) {
+    const [message, setMessage] = useState("");
+    
+    const handleClick = async () => {
+      try {
+        const response = await fetch(`/api/outfit/${productId.productId}`, {
+          method: "DELETE",
+        });
+        if (response.ok) {
+          const data = await response.json();
+          setMessage(data.message); // Armazena a mensagem do backend no estado
+          console.log(data);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+      
+    };
+  
+    
+    return (
+      <div>
+        <button onClick={handleClick} className="outFit">
+          Remove Outfit
+        </button>
+        {message && <p>{message}</p>}
+      </div>
+    );
+  }
+
 
 export const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -72,13 +117,13 @@ export const SearchBar = () => {
       const response = await fetch(`/api/products/search?q=${searchTerm}`, {
         method: "GET",
       });
-      console.log(response.json());
+     
       if (response.ok) {
         const data = await response.json();
-        console.log(data);
+        console.log(data)
+        return data
 
-        //   } else {
-        //     throw new Error("SearchBox not found");
+        
       }
     } catch (err) {
       console.log(err);
@@ -98,5 +143,193 @@ export const SearchBar = () => {
         Search{" "}
       </button>
     </form>
+  );
+};
+
+export const FetchAllProductsButton = () => {
+  const [products, setProducts] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleClick = async () => {
+    try {
+      const response = await fetch("/api/products", {
+        method: "GET",
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setProducts(data);
+        setErrorMessage("");
+      } else {
+        const errorData = await response.json();
+        setErrorMessage(errorData.message);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>Fetch All Products</button>
+      {errorMessage && <p>{errorMessage}</p>}
+      {products.length > 0 && (
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>{product.name}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+export const Sidebar = () => {
+  const [selectedMaterial, setSelectedMaterial] = useState("");
+  const [selectedColor, setSelectedColor] = useState("");
+  const [selectedSex, setSelectedSex] = useState("");
+  const [selectedRating, setSelectedRating] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const handleMaterialChange = (event) => {
+    setSelectedMaterial(event.target.value);
+  };
+
+  const handleColorChange = (event) => {
+    setSelectedColor(event.target.value);
+  };
+
+  const handleSexChange = (event) => {
+    setSelectedSex(event.target.value);
+  };
+
+  const handleRatingChange = (event) => {
+    setSelectedRating(event.target.value);
+  };
+
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  //Endpoints
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch(
+        `/api/products/category/${selectedCategory}`, {
+            method: "GET",
+          })
+      ;
+      if (response.ok) {
+        const data = await response.json();
+        console.log("Category:", data.category);
+      }
+
+      // Endpoint para filtro de cor
+      const colorResponse = await fetch(`/api/products/color/${selectedColor}`, {
+        method: "GET",
+      });
+      if (colorResponse.ok) {
+        const colorData = await colorResponse.json();
+        console.log("Color:", colorData.color);
+      }
+      // Endpoint para filtro de sexo
+      const sexResponse = await fetch(`/api/products/genders/${selectedSex}`, {
+        method: "GET",
+      });
+      if (sexResponse.ok) {
+        const sexData = await sexResponse.json();
+        console.log("Sex:", sexData.color);
+      }
+
+      // Endpoint para filtro de material
+      const materialResponse = await fetch(
+        `/api/products/material/${selectedMaterial}`, {
+            method: "GET",
+          });
+      if (materialResponse.ok) {
+        const materialData = await materialResponse.json();
+        console.log("Material:", materialData.material);
+      }
+
+      // Endpoint para filtro de rating(melhores produtos)
+      const ratingResponse = await fetch(
+        `/api/products/material/${selectedRating}`, {
+            method: "GET",
+          })
+      ;
+      if (ratingResponse.ok) {
+        const ratingData = await ratingResponse.json();
+        console.log("Material:", ratingData.rating);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Filters</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="filter">
+          <label htmlFor="material">Material:</label>
+          <select
+            id="material"
+            value={selectedMaterial}
+            onChange={handleMaterialChange}
+          >
+            <option value="">All</option>
+            <option value="cotton">Algodão</option>
+            <option value="wool">Lã</option>
+            <option value="polyester">100% Algodão Orgânico</option>
+          </select>
+        </div>
+        <div className="filter">
+          <label htmlFor="color">Cor:</label>
+          <select id="color" value={selectedColor} onChange={handleColorChange}>
+            <option value="">All</option>
+            <option value="red">Vermelho</option>
+            <option value="blue">Bege</option>
+            <option value="green">Marrom</option>
+          </select>
+        </div>
+        <div className="filter">
+          <label htmlFor="sex">Sexo:</label>
+          <select id="sex" value={selectedSex} onChange={handleSexChange}>
+            <option value="">All</option>
+            <option value="male">Masculino</option>
+            <option value="female">Feminino</option>
+            <option value="unissex">Unissex</option>
+          </select>
+        </div>
+        <div className="filter">
+          <label htmlFor="category">Category:</label>
+          <select
+            id="category"
+            value={selectedCategory}
+            onChange={handleCategoryChange}
+          >
+            <option value="">All</option>
+            <option value="coat">Casaco</option>
+            <option value="shoes">Sapato</option>
+            <option value="dress">Vestido</option>
+            <option value="hat">Bone</option>
+          </select>
+        </div>
+
+        <div className="filter">
+          <label htmlFor="category">Melhores Products:</label>
+          <select
+            id="category"
+            value={selectedCategory}
+            onChange={handleRatingChange}
+          >
+            <option value="">All</option>
+          </select>
+        </div>
+        <button onClick={handleSubmit} type="submit">
+          Apply Filters
+        </button>
+      </form>
+    </div>
   );
 };
