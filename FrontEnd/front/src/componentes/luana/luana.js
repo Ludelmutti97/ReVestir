c4c2cdc6d9f6e32840a2c2b512f4a2a97dc0e7c0
Luanna Delmuti <ludelmutti@gmail.com>
2023-06-06 11:27:21 +0100

    fwfewfew

--- FrontEnd/front/src/componentes/luana/luana.js
+++ FrontEnd/front/src/componentes/luana/luana.js
@@ -1,10 +1,57 @@
-
+import { useState } from 'react'
 import React from 'react';
-import { Link } from 'react-router-dom';
+import './luana.css'
+import {Link} from 'react-router-dom';
 
 function Luana (){
+    const [isFavorite, setIsFavorite] = useState(false);
+
+  const handleClick = async () => {
+    try {
+      const response = await fetch('/api/favorite/:id');
+      if (response.ok) {
+        const data = await response.json();
+        setIsFavorite(true);
+        return data
+      } else {
+        throw new Error('not_found');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+
+    <div className='favorite-button-wrapper'>
+    <button onClick={handleClick}  className="favorite-button">
+      {isFavorite ? 'Favorited' : '❤️'}
+    </button>
+    </div>
+  )
+
+  }
+
+const AddToOutfitButton = ({ productId }) => {
+    const handleClick = async () => {
+      try {
+        const response = await fetch(`/api/outfit/${productId}`, {
+          method: 'POST'
+        });
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data); // Aqui você pode manipular a resposta do backend
+        } else {
+          throw new Error('Erro');
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  
     return (
-        <h1>Hello Luana</h1>
-    )
-}
+      <button onClick={handleClick}>Add to Outfit</button>
+    );
+  }
+
 export default Luana
